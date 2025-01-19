b54a8d9f2258804f25c0160cfcfc320850d4df7b
Maqsud <maqsuderjonov@gmail.com>
2025-01-20 00:54:22 +0500

    files

--- downloader.sh
+++ downloader.sh
@@ -4,8 +4,18 @@ BASE_PATH_COPY_TO=
 BASE_SUB_URI=
 FILES_FOR_COPY=
 
-https://github.com/maqsud-dev/web-files/tree/main/examples?noancestors=1
-https://github.com/maqsud-dev/web-files/tree/main/examples/folder-02?noancestors=1
+BASE_URL=https://$TOKEN@raw.githubusercontent.com/maqsud-dev/web-files/refs/heads/main/
+curl -s https://$TOKEN@raw.githubusercontent.com/<user or organization>/<repo name>/<branch>/<path to file>/<file_name>
+
+# You can use the V3 API to get a raw file like this (you'll need an OAuth token):
+curl -H 'Authorization: token INSERTACCESSTOKENHERE'  -H 'Accept: application/vnd.github.v3.raw' \
+  -O  -L https://api.github.com/repos/owner/repo/contents/path
+# All of this has to go on one line. The -O option saves the file in the current directory. You can use -o filename to specify a different filename.
+
+
+
+# https://github.com/maqsud-dev/web-files/tree/main/examples?noancestors=1
+# https://github.com/maqsud-dev/web-files/tree/main/examples/folder-02?noancestors=1
 
 curl -H "Accept: application/json" https://github.com/maqsud-dev/web-files/tree/main/examples?noancestors=1
 
